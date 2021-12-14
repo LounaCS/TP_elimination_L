9fb1515ced996ab5188a16e4e307f4204a255005
Lou <louna.colaert.s@gmail.com>
2021-12-14 16:36:40 +0000

    further reorganisation of files

---
+++ scripts/processed_1_download_cds_belari.sh
@@ -0,0 +1,5 @@
+mkdir -p data/cds_belari
+
+wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS16/species/mesorhabditis_belari/PRJEB30104/mesorhabditis_belari.PRJEB30104.WBPS16.CDS_transcripts.fa.gz  -P data/cds_belari
+
+gunzip data/cds_belari/mesorhabditis_belari.PRJEB30104.WBPS16.CDS_transcripts.fa.gz
