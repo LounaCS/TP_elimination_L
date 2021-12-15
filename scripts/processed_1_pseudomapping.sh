5a07d27e37f449b063158b153f9e3e96a8efff26
Lou <louna.colaert.s@gmail.com>
2021-12-15 08:38:31 +0000

    script for pseudomapping (kallipso)

---
+++ scripts/processed_1_pseudomapping.sh
@@ -0,0 +1,25 @@
+mkdir -p ~/mydatalocal/TP_elimination_L/3_processed_data/quantification/
+
+INDEX=~/mydatalocal/TP_elimination_L/2_preprocessed_data/intermediary_results/quantification/index_belari/mesorhabditis_belari.index
+LEN=522 # fragment length
+SD=200 # standard deviation, that kallisto uses to correct the mapping success given the length of the fragments
+
+for TRIM_FASTQ_GZ_FILE in ~/mydatalocal/TP_elimination_L/2_preprocessed_data/intermediary_results/trim_data/*gz
+do
+  TRIM_FASTQ_GZ_NAME=$(basename $TRIM_FASTQ_GZ_FILE)
+  TRIM_FASTQ_GZ_PREFIX=${TRIM_FASTQ_GZ_NAME/_trim.fastq.gz}
+  echo $TRIM_FASTQ_GZ_FILE
+  echo $TRIM_FASTQ_GZ_NAME
+  echo $TRIM_FASTQ_GZ_PREFIX
+  
+  OUT_QUANT_PREFIX="~/mydatalocal/TP_elimination_L/3_processed_data/quantification/${TRIM_FASTQ_GZ_PREFIX}"
+  echo $OUT_QUANT_PREFIX
+  echo $OUT_REPORTS_PREFIX
+  
+  mkdir -p "~/mydatalocal/TP_elimination_L/3_processed_data/quantification/${TRIM_FASTQ_GZ_PREFIX}/"
+  kallisto quant -i ${INDEX} -t 7 \
+                 --single -l ${LEN} -s ${SD} \
+                 --bias \
+                 -o ${OUT_QUANT_PREFIX} \
+                 ${TRIM_FASTQ_GZ_FILE} &> ${OUT_QUANT_PREFIX}_kallisto_mapping_report.txt
+done
