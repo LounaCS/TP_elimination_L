f9de338628fd3b0e2e9ad9c9bf36b41b08468c6f
Lou <louna.colaert.s@gmail.com>
2021-12-15 08:42:47 +0000

    code to aggregate the pseudo mapping results of all 12 samples in one file: table of counts

---
+++ scripts/processed_2_aggregating_report.sh
@@ -0,0 +1,4 @@
+ results = ~/mydatalocal/TP_elimination_L/3_processed_data/quantification/
+
+multiqc -f ~/mydatalocal/TP_elimination_L/3_processed_data/quantification//quantification/ \
+-o ~/mydatalocal/TP_elimination_L/3_processed_data/quantification//multiqc_report_quantification/
