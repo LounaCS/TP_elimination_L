fd28356ec6317fbf0a8f3b36c403bf5e9f8f5e37
Lou <louna.colaert.s@gmail.com>
2021-12-15 08:37:47 +0000

    scripts for constricting the index for the pseudomapping

--- scripts/preprocessed_5_download_cds_belari.sh
+++ scripts/preprocessed_5_download_cds_belari.sh
@@ -1,5 +1,6 @@
 mkdir -p 2_preprocessed_data/cds_belari
 
-wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS16/species/mesorhabditis_belari/PRJEB30104/mesorhabditis_belari.PRJEB30104.WBPS16.CDS_transcripts.fa.gz  -P data/cds_belari
+wget https://ftp.ebi.ac.uk/pub/databases/wormbase/parasite/releases/WBPS16/species/mesorhabditis_belari/PRJEB30104/mesorhabditis_belari.PRJEB30104.WBPS16.CDS_transcripts.fa.gz  \
+-P ~/mydatalocal/TP_elimination_L/2_preprocessed_data/cds_belari
 
-gunzip data/cds_belari/mesorhabditis_belari.PRJEB30104.WBPS16.CDS_transcripts.fa.gz
+gunzip ~/mydatalocal/TP_elimination_L/2_preprocessed_data/cds_belari/mesorhabditis_belari.PRJEB30104.WBPS16.CDS_transcripts.fa.gz
--- scripts/preprocessed_6_index_construction.sh
+++ scripts/preprocessed_6_index_construction.sh
@@ -1,7 +1,7 @@
-mkdir -p intermediary_results/quantification/index_belari
+mkdir -p ~/mydatalocal/TP_elimination_L/2_preprocessed_data/intermediary_results/quantification/index_belari
 
-CDS_FASTA=data/cds_belari/mesorhabditis_belari.PRJEB30104.WBPS16.CDS_transcripts.fa
-INDEX_OUT_PREFIX=intermediary_results/quantification/index_belari/mesorhabditis_belari
+CDS_FASTA=~/mydatalocal/TP_elimination_L/2_preprocessed_data/cds_belari/mesorhabditis_belari.PRJEB30104.WBPS16.CDS_transcripts.fa
+INDEX_OUT_PREFIX=~/mydatalocal/TP_elimination_L/2_preprocessed_data/intermediary_results/quantification/index_belari/mesorhabditis_belari
 
 kallisto index -k 31 --make-unique -i ${INDEX_OUT_PREFIX}.index ${CDS_FASTA} \
 2> ${INDEX_OUT_PREFIX}_kallisto_index_report.txt
