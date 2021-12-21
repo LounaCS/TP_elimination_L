1477400b0da282a1b56941c867e64fc7bc58285f
LounaCS <79788037+LounaCS@users.noreply.github.com>
2021-12-21 16:16:51 +0100

    removed the string manipulation
    not used here, redundant with preprocessed_3_cleaning_data script

--- scripts/preprocessed_2_quality_check.sh
+++ scripts/preprocessed_2_quality_check.sh
@@ -9,14 +9,3 @@ do
   
   fastqc $FASTQ_GZ_FILES -o intermediary_results/raw_quality -t 7
 done
-
-
-# test of string manipulation to rename files
-for FASTQ_GZ_FILES in data/fastq_files/*gz
-do
-  FASTQ_GZ_NAME=$(basename $FASTQ_GZ_FILES)
-  FASTQ_GZ_PREFIX=${FASTQ_GZ_NAME/.fastq.gz}
-  echo $FASTQ_GZ_FILES
-  echo $FASTQ_GZ_NAME
-  echo $FASTQ_GZ_PREFIX
-done
